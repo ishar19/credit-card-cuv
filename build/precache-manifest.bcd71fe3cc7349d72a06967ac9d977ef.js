ca82f9d856d80856c835a8965040e2bcb967f183
ishar <isharjain19@gmail.com>
2023-06-14 21:04:03 +0530

    added build

---
+++ build/precache-manifest.bcd71fe3cc7349d72a06967ac9d977ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "66e844e7cd677a0564bf",
+    "url": "/interactive-card/static/css/main.83c42058.chunk.css"
+  },
+  {
+    "revision": "66e844e7cd677a0564bf",
+    "url": "/interactive-card/static/js/main.66e844e7.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/interactive-card/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "a40fc0b28868594de4b0",
+    "url": "/interactive-card/static/js/2.a40fc0b2.chunk.js"
+  },
+  {
+    "revision": "5ac5bf55b64b0cc6c6a7",
+    "url": "/interactive-card/static/js/runtime~main.5ac5bf55.js"
+  },
+  {
+    "revision": "1a41a92ca7b04ab1362f1b2f6bb2a83a",
+    "url": "/interactive-card/static/media/bg-card-back.1a41a92c.png"
+  },
+  {
+    "revision": "33d6bbef0946f7071752c239b14d4a67",
+    "url": "/interactive-card/static/media/bg-card-front.33d6bbef.png"
+  },
+  {
+    "revision": "3b8e5db1428efb79aa0006dfbef1d690",
+    "url": "/interactive-card/static/media/bg-main-mobile.3b8e5db1.png"
+  },
+  {
+    "revision": "530df842c362d952de57b6d262361efc",
+    "url": "/interactive-card/static/media/icon-complete.530df842.svg"
+  },
+  {
+    "revision": "f20ceb8b54aa810d20612404c18f3dd7",
+    "url": "/interactive-card/index.html"
+  }
+];
